de699f8b99a1704bd35224eff2d414b7f094bc51
DayraGramiscelli <131476671+DayraGramiscelli@users.noreply.github.com>
2024-03-29 09:59:16 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,84 @@
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-        char ch;
-        ch = 'X';
-        System.out.println("ch contains " + ch);
-        ch++; //incrementa ch
-        System.out.println("ch is now " + ch);
-        ch = 90; // dá a ch valor Z
-        System.out.println("ch is now " + ch);
+        Scanner scanner = new Scanner(System.in); // Receber dados do cliente
+        Random valorAleatorio = new Random(); // Gerar número aleatorio 0 a 100.
 
-        byte tipoByte = 127;
-        short tipoShort = 32767;
-        char tipoChar = 'C';
-        float tipoFloat = 2.6f;
-        double tipoDouble = 3.59;
-        int tipoInt = 2147483647;
-        long tipoLong = 9223372036854775807L;
-        boolean tipoBooleano = true;
-        System.out.println("Valor do tipoByte = " + tipoByte);
-        System.out.println(" Valor do tipoShort = " + tipoShort);
-        System.out.println(" Valor do tipoChar = " + tipoChar);
-        System.out.println(" Valor do tipoFloat = " + tipoFloat);
-        System.out.println(" Valor do tipoDouble = " + tipoDouble);
-        System.out.println(" Valor do tipoInt = " + tipoInt);
-        System.out.println(" Valor do tipoLong = " + tipoLong);
-        System.out.println(" Valor do tipoBooleano = " + tipoBooleano);
-        System.out.println("Exemplo do while");
-        int opcao = -1;
-        Scanner in = new Scanner(System.in);
+        // Variaveis atribuidas.
+        int menu = 0;
+        int valor = valorAleatorio.nextInt(100);
+        char letraPremiada = 'D';
+        int numeroPar = 0;
+
+        //Menu do Sistema
         do {
-            System.out.println("******** Sistema de Cadastro ********");
-            System.out.println("Opção 1: Cadastrar cliente");
-            System.out.println("Opção 2: Cadastrar produto");
-            System.out.println("Opção 3: Cadastrar vendedor");
-            System.out.println("Opção 0: Sair");
-            System.out.println("**************************************");
-            System.out.print("Digite a opção: ");
-            opcao = in.nextInt();
-            System.out.println("**************************************");
-            switch (opcao){
+            System.out.println("=================================");
+            System.out.println("      MENU LOTO FACIL ");
+            System.out.println(" 1 - Apostar de 0 a 100");
+            System.out.println(" 2 - Apostar de A à Z");
+            System.out.println(" 3 - Apostar em par ou ímpar");
+            System.out.println(" 0 - Sair do programa");
+            System.out.println("=================================");
+            System.out.println(" Digite uma Opção: ");
+            menu = scanner.nextInt();
+
+
+            // Case para entrar dentro do paremetro escolhido, logo após excutar uma função
+
+            switch (menu) {
                 case 1:
-                    System.out.println("Opção 1 escolhida.");
-                    // cadastrar Cliente();
+                    System.out.println("Escolha um número:  ");
+                    Scanner scanner1 = new Scanner(System.in);
+                    int numeroEscolhido = scanner1.nextInt();
+                    if (numeroEscolhido < 0 && numeroEscolhido > 100 || numeroEscolhido == valor) {
+                        System.out.println("Você ganhou R$ 1.000,00 reais.");
+                    } else {
+                        System.out.println("Que pena o número sorteado foi: " + valor);
+                    }
                     break;
                 case 2:
-                    System.out.println("Opção 2 escolhida.");
-                    // cadastrar Produto();
+                    System.out.println("Escolha uma letra entre A a Z: ");
+                    try {
+                        int input = System.in.read();
+                        char letraDigitada = Character.toUpperCase((char) input);
+
+                        if (Character.isLetter(input)) {
+                            if (letraDigitada == letraPremiada) {
+                                System.out.println("Você ganhou R$ 500,00 reais.");
+                            } else {
+                                System.out.println("Que pena! A letra sorteada foi: " + letraPremiada + ".");
+                            }
+                        } else {
+                            System.out.println("Aposta inválida.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case 3:
-                    System.out.println("Opção 3 escolhida.");
-                    // cadastrar Vendedor();
+                    System.out.println("Digite um número");
+                    Scanner scanner2 = new Scanner(System.in);
+                    int numeroEscolhido2 = scanner2.nextInt();
+                    if (numeroEscolhido2 % 2 == 0) {
+                        System.out.println("Você ganhou R$ 100,00 reais.");
+                    }
+                    else {
+                        System.out.println("Que pena! O número digitado é ímpar e a premiação foi para números pares.");
+                    }
                     break;
                 case 0:
-                    System.out.println("Opção 0 escolhida.");
-                    // Sair();
+                    System.out.println("Jogo Finalizado...");
                     break;
+
                 default:
-                    System.out.println("O número escolhido é inválido!");
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+
             }
-        }while(opcao != 0);
+        } while (menu != 0);
+
     }
 
 
