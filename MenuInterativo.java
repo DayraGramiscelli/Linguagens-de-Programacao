b83ff4dffb8dc8a85ca1936d91c10bb85b5a5921
DayraGramiscelli <131476671+DayraGramiscelli@users.noreply.github.com>
2024-03-31 16:33:28 -0300

    Add files via upload

---
+++ MenuInterativo.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class MenuInterativo {
+    private JPanel Menu;
+    private JLabel Titulo;
+    private JLabel ApostaNum;
+    private JLabel ApostaLetra;
+    private JLabel ApostaPOuI;
+    private JTextField CampoDeAposta1;
+    private JTextField CampoDeAposta2;
+    private JTextField CampoDeAposta3;
+    private JButton Sair;
+
+    private void createUIComponents() {
+        private void jButtonSairActionPerformed(java.awt.event.ActionEvent) {
+            // Fechar o programa quando o botão "Sair" for pressionado
+            System.exit(Sair);
+        }
+        // TODO: place custom component creation code here
+    }
+}
